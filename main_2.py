62ffaf8396e9c427f90c08e6fa668f6b06423024
LiuMin2021 <90753004+LiuMin2021@users.noreply.github.com>
2022-05-10 23:35:38 +0200

    Update main_2.py

--- main_2.py
+++ main_2.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
-import sys
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QToolTip, QVBoxLayout, QHBoxLayout, QLabel, QLCDNumber, QDialog, QLineEdit, QGridLayout, QFormLayout,QTextEdit,QRadioButton, QCheckBox, QSlider
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLCDNumber, QCheckBox, QSlider
 from PyQt5.QtCore import Qt, QSize, QTimer
-from gpiozero import LED
+from gpiozero import LED, LEDBoard
 from signal import pause
+
 
 # Klasse für das Hauptfenster
 class MyWindow(QMainWindow):
     def __init__(self):# Slider + Label Anzeige Dezimalwert
         # Konstruktor von QMainWindow aufrufen
         super().__init__()
-    
-        self.setMinimumSize(QSize(500, 400))   
+        
+        self.leds = LEDBoard(18, 23, 24, 25)
+        self.setMinimumSize(QSize(500, 300))   
         self.setWindowTitle('Dezimal-Binär-Rechner') 
         self.setStyleSheet("background-color: rgb(1, 22, 200)")        
         wid = QWidget(self) 
@@ -24,18 +25,17 @@ class MyWindow(QMainWindow):
         self.lcd_number = QLCDNumber(2, wid) #LCD Nummber
         vlayout.addWidget(self.lcd_number)
         
-        hlayout = QHBoxLayout() #widgets
-        vlayout.addLayout(hlayout)
-            
+                   
         # Slider + Label Anzeige Dezimalwert ,  Layout(sliderbox)
-        self.slider = QSlider(Qt.Horizontal)
+        self.slider = QSlider(Qt.Horizontal,wid)
         self.slider.setMinimum(0)
         self.slider.setMaximum(15)
-        self.slider.setSingleStep(1)
+        #self.slider.setSingleStep(1)
         self.slider.setValue(5)
         self.slider.setTickPosition(QSlider.TicksBelow)
         self.slider.setTickInterval(1)
-        vlayout.addWidget(self.slider)
+        #vlayout.addWidget(self.slider)
+        self.slider.valueChanged.connect(self.dec2bin)
         
         self.label = QLabel('0')  
         self.label.setAlignment(Qt.AlignCenter)
@@ -46,14 +46,16 @@ class MyWindow(QMainWindow):
         sliderbox = QHBoxLayout()
         sliderbox.addWidget(self.slider)
         sliderbox.addWidget(self.label)
-        
 
         # Labels fuer 4 Bits , Layout(bitbox)
         self.bitlabels = [QLabel("8"),QLabel("4"),QLabel("2"),QLabel("1")] # Liste 
+        self.length=len(self.bitlabels)
+        print (self.length)
+        
         # hier bitlabels erstellen      
         bitbox = QHBoxLayout()
-        for bitlabel in self.bitlabels:
-            bitbox.addWidget(bitlabel)
+        
+       
 
         # Layout zusammenbauen
         vbox = QVBoxLayout()
@@ -63,12 +65,27 @@ class MyWindow(QMainWindow):
         # vbox anzeigen in QWidget
         self.setLayout(vbox)
         
-    def dec2bin(self):
-        self.label.setText(str(value))    
+        for index, bitlabel in enumerate(self.bitlabels):
+            bitbox.addWidget(bitlabel)            
+            bitlabel.setStyleSheet("background-color: rgb(100, 100, 100)")
+            bitlabel.setFixedWidth(20)
+            bitlabel.setFixedHeight(20)
+            bitlabel.setAlignment(Qt.AlignCenter)
         
+    def dec2bin(self,value):
+        self.label.setText(str(value)) 
+        for j in range(self.length):
+            print (j)
+            if (value & 1<<j):        
+                self.bitlabels[self.length-1-j].setStyleSheet("background-color: rgb(255, 0, 0)")
+                self.leds[j].on()            
+            else:
+                self.bitlabels[self.length-1-j].setStyleSheet("background-color: rgb(100, 100, 100)")
+                self.leds[j].off() 
    
 
 app = QtWidgets.QApplication([])
 win = MyWindow()
 win.show()
 app.exec_()
+
